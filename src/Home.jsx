e152338cf56f1fcec03060fbae37edd20a08c9c6
Santhoshi Nanikeri <121660008+santhoshi-vijaykumar-nanikeri@users.noreply.github.com>
2026-03-30 12:25:43 +0000

    Added shadcn.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 function Home() {
   return (
     <h1>
       Welcome home! <div className="text-red-500 text-2xl">Hello</div>{" "}
+      <div className="p-6">
+        <Button variant="outline">Working</Button>
+      </div>
     </h1>
   );
 }
